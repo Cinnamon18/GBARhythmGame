a53d5b97faf5f4d501f121ff8b35239df01271c3
Cinnamon <alice.ava.moh@gmail.com>
2018-11-20 21:03:19 -0800

    last time, i swear

--- beatmaps.h
+++ beatmaps.h
@@ -1,11 +1,11 @@
 #ifndef BEATMAPS_SEEN
 #define BEATMAPS_SEEN
 
-extern const unsigned short map1[23];
-#define MAP1_SIZE 23
+extern const unsigned short map1[43];
+#define MAP1_SIZE 43
 
-extern const unsigned short map2[30];
-#define MAP2_SIZE 30
+extern const unsigned short map2[40];
+#define MAP2_SIZE 40
 
 extern const unsigned short map3[31];
 #define MAP3_SIZE 31
